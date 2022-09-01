4815fa17238ff1e601dbd69c28ca32e3e4967dbd
hun <hunea6686@naver.com>
2022-09-01 17:37:51 +0900

    4주차[20Day]

---
+++ 4주차/20Day/src/main.js
@@ -0,0 +1,20 @@
+const DUMMY_DATA = [
+    {
+        id: 1,
+        title: '테스트 1'
+    },
+    {
+        id: 2,
+        title: '테스트 2'
+    },
+    {
+        id: 3,
+        title: '테스트 3'
+    },
+    {
+        id: 4,
+        title: '테스트 4'
+    },
+]
+
+const $target = document.querySelector('#app')
