2ee2bf98e4c0aeba07d14155c787fd4c41cc9315
hun <hunea6686@naver.com>
2022-09-02 16:47:16 +0900

    4주차[20Day]

--- 4주차/20Day/src/Editor.js
+++ 4주차/20Day/src/Editor.js
@@ -1,9 +1,10 @@
 export default function Editor({ $target, inititalState = {
     title: '',
     content: ''
-} }) {
+}, onEditing }) {
     const $editor = document.createElement('div')
 
+    let isinitialize = false
     this.state = inititalState
    
     $target.appendChild($editor)
@@ -14,18 +15,31 @@ export default function Editor({ $target, inititalState = {
     }
 
     this.render = () => {
-       $editor.innerHTML = `
-        <input type="text" name="title" style="width:600px;" value="${this.state.title}"/>
-        <textarea name="content" style="width:600px; height:400px;">${this.state.content}</textarea>
-         `
+        if(!isinitialize) {
+            $editor.innerHTML = `
+                <input type="text" name="title" style="width:600px;" value="${this.state.title}"/>
+                <textarea name="content" style="width:600px; height:400px;">${this.state.content}</textarea>
+                `
+
+        isinitialize = true
+        }
     }
 
     this.render()
 
     $editor.addEventListener('keyup', e => {
+        const { target } = e
+
+        const name = target.getAttribute('name')
         
-        this.state = {
-            ...this.state
+        if(this.state[name]) {
+            const nextState = {
+                ...this.state,
+                [name]: target.value
+            }
+
+           this.setState(nextState)
+           onEditing(this.state)
         }
     })
 }
--- 4주차/20Day/src/main.js
+++ 4주차/20Day/src/main.js
@@ -6,7 +6,13 @@ const $target = document.querySelector('#app')
 
 // new App({ $target })
 
-new Editor ({ $target, inititalState: {
-    title: '오늘의 학습일지',
-    content: 'TypeScript'
-} })
+new Editor ({ 
+    $target,
+    inititalState: {
+        title: '오늘의 학습일지',
+        content: 'TypeScript'
+    },
+    onEditing: (post) => {
+        console.log(post)
+    } 
+})
