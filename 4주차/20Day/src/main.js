fd6a0abb52fbbb90b690c6d8554ce0036275d280
hun <hunea6686@naver.com>
2022-09-02 17:04:46 +0900

    4주차[20Day]

--- 4주차/20Day/src/main.js
+++ 4주차/20Day/src/main.js
@@ -1,10 +1,20 @@
 import App from "./App.js"
 import Editor from "./Editor.js"
+import { getItem, setItem } from "./storage.js"
 
 const $target = document.querySelector('#app')
 
 
 // new App({ $target })
+
+const TEMP_POST_SAVE_KEY = 'temp-post'
+
+const posts = getItem(TEMP_POST_SAVE_KEY, {
+    title:'',
+    content:''
+})
+
+console.log(posts)
 
 new Editor ({ 
     $target,
@@ -13,6 +23,9 @@ new Editor ({
         content: 'TypeScript'
     },
     onEditing: (post) => {
-        console.log(post)
+        setItem('temp-TEMP_POST_SAVE_KEY', {
+            ...post,
+            tempSaveDate: new Date()
+        })
     } 
 })
